fccafcb6bb0797fdca05303615b32a251acc97c1
Matthew Bratrsovsky <realzing@pm.me>
2024-08-16 10:28:11 -0600

    [nb] Edit: FullScreen.tsx

--- FullScreen.tsx
+++ FullScreen.tsx
@@ -1,22 +1,25 @@
 export const FullScreen = () => {
-	const fullScreenRef = useRef<HTMLElement>(null);
+	const fullscreenRef = useRef<HTMLElement>(null);
 
 	const fullscreen = useRef(false);
 
 	useEffect(() => {
 		if (
 			typeof document === "undefined" ||
-			!("requestFullscreen" in document) ||
-			!fullScreenRef.current
+			!(
+				"requestFullscreen" in document &&
+				typeof document.requestFullscreen === "function"
+			) ||
+			!fullscreenRef.current
 		)
 			return;
 
 		if (fullscreen) {
-			document["requestFullscreen"](fullScreenRef.current);
+			document.requestFullscreen(fullscreenRef.current);
 		} else {
 			document.exitFullscreen();
 		}
 	}, [fullscreen]);
 
-	return <div ref={fullScreenRef} />;
+	return <div ref={fullscreenRef} />;
 };
